a1719bd51736b9ca0ce9a6d3fc201f1e40bdd648
visotskydata <129074755+visotskydata@users.noreply.github.com>
2026-02-13 18:40:38 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // js/config.js
-export const SUPABASE_URL = 'ТВОЙ_URL_СЮДА';
-export const SUPABASE_KEY = 'ТВОЙ_ANON_KEY_СЮДА';
+export const SUPABASE_URL = 'https://baxaxcsvbkgfuwysabrd.supabase.co';
+export const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJheGF4Y3N2YmtnZnV3eXNhYnJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA5ODMyNzgsImV4cCI6MjA4NjU1OTI3OH0.3RDpU3cG0R4kMqRtDJlZk5uhG2jwfCK1F-UprZ-aUkk';
 
 export const GAME_SETTINGS = {
     updateInterval: 1000, // Частота обновления сервера
