2eab2e6e731e4dc145a8ea02aed0860fea69a79f
visotskydata <129074755+visotskydata@users.noreply.github.com>
2026-02-13 18:41:49 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+// js/main.js
+import { db } from './core/db.js';
+import { initUI } from './ui/render.js';
+// В будущем импортируем чат, бой и т.д.
 
+console.log('Игра запускается...');
+
+// Глобальное состояние игры
+window.gameState = {
+    currentUser: null,
+    players: []
+};
+
+// Функция старта
+async function initGame() {
+    initUI(); // Рисуем интерфейс
+    console.log('Системы готовы');
+}
+
+// Запуск
+initGame();
