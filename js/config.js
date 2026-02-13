f34a1be7a5b90405cda4f527ba5c328debf339bc
visotskydata <129074755+visotskydata@users.noreply.github.com>
2026-02-13 18:44:41 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,17 @@
 // js/config.js
-export const SUPABASE_URL = 'https://baxaxcsvbkgfuwysabrd.supabase.co';
+
+// ВСТАВЬ СЮДА СВОИ КЛЮЧИ!
+export const SUPABASE_URL = 'https://baxaxcsvbkgfuwysabrd.supabase.co'; 
 export const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJheGF4Y3N2YmtnZnV3eXNhYnJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA5ODMyNzgsImV4cCI6MjA4NjU1OTI3OH0.3RDpU3cG0R4kMqRtDJlZk5uhG2jwfCK1F-UprZ-aUkk';
 
 export const GAME_SETTINGS = {
-    updateInterval: 1000, // Частота обновления сервера
-    renderInterval: 100,  // Частота перерисовки (FPS)
-    mapWidth: 800,
-    mapHeight: 600
+    updateInterval: 1000, // Синхронизация с сервером (мс)
+    renderInterval: 100,  // Перерисовка (FPS)
+};
+
+export const SKINS = {
+    'warrior': '⚔️',
+    'mage': '🧙‍♂️',
+    'archer': '🏹',
+    'default': '❓'
 };
