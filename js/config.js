4021d004c2c4a200dd89fa7417407277a066b017
visotskydata <129074755+visotskydata@users.noreply.github.com>
2026-02-13 18:40:18 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,10 @@
+// js/config.js
+export const SUPABASE_URL = 'ТВОЙ_URL_СЮДА';
+export const SUPABASE_KEY = 'ТВОЙ_ANON_KEY_СЮДА';
 
+export const GAME_SETTINGS = {
+    updateInterval: 1000, // Частота обновления сервера
+    renderInterval: 100,  // Частота перерисовки (FPS)
+    mapWidth: 800,
+    mapHeight: 600
+};
